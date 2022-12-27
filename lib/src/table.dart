3a1eca4dd96b7e41b03dfef0df218baf28c998c4
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2022-12-27 04:16:06 -0500

    add function insert and insertAll

--- lib/src/table.dart
+++ lib/src/table.dart
@@ -1,16 +1,49 @@
+import 'package:query_builder/src/exec.dart';
+import 'package:query_builder/src/sql_query.dart';
 import 'package:query_builder/src/where.dart';
 
 class Table extends Data with Where {
-  Table(
-    this.tableName,
-  );
+  Table(this.tableName);
 
   final String tableName;
 
   static final query = <String>[];
 
-  void insert(List<Map<String, dynamic>> value) {
-    UnimplementedError('function INSERT not implement');
+  Exec insert(Map<String, dynamic> value) {
+    final sql = SQLquery.instance;
+    sql.query.add('INSERT INTO ${sql.table}');
+    sql.query.add('(${value.keys.join(', ')})');
+
+    final values = <String>[];
+    value.forEach((key, value) {
+      final p0 = sql.paramsCode;
+      sql.params[p0] = value;
+      values.add('@$p0');
+    });
+
+    sql.query.add('VALUES (${values.join(', ')})');
+
+    return Exec();
+  }
+
+  Exec insertAll(List<JMap> values) {
+    final sql = SQLquery.instance;
+    sql.query.add('INSERT INTO ${sql.table}');
+    sql.query.add('(${values.first.keys.join(', ')})');
+    sql.query.add('VALUES');
+
+    final listValues = <dynamic>[];
+    for (final item in values) {
+      final values = <String>[];
+      item.forEach((key, value) {
+        final p0 = sql.paramsCode;
+        sql.params[p0] = value;
+        values.add('@$p0');
+      });
+      listValues.add('(${values.join(', ')})');
+    }
+    sql.query.add('${listValues.join(', ')};');
+    return Exec();
   }
 
   Where select() {
