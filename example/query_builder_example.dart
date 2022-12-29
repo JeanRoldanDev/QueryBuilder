271ed679add57da2ef67fccb2f039ee9683f14ad
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2022-12-28 22:59:58 -0500

    update main example

--- example/query_builder_example.dart
+++ example/query_builder_example.dart
@@ -1,6 +1,23 @@
-import 'package:query_builder/query_builder.dart';
+import 'package:database_query_builder/query_builder.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+Future<void> main() async {
+  DB.conexion(
+    databaseName: 'dartpay',
+    port: 5442,
+  );
+
+  //=========================================================
+  //==================> DATA METHODS Statements <=============
+  // await DataMethods.get();
+  // await DataMethods.getModel();
+  // await DataMethods.toSql();
+
+  //=========================================================
+  //==================> INSERT INTO Statements <=============
+  // await InsertStatements.insert();
+  // await InsertStatements.insertAll();
+
+  //=========================================================
+  //==================> SELECT Statements <==================
+  // await SelectStatements.select();
 }
