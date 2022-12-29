9a4fc1907b4d276d117a8f3d7c96fc7e5a7fe34e
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2022-12-28 22:59:45 -0500

    update select statements example

--- example/select_statements.dart
+++ example/select_statements.dart
@@ -4,9 +4,38 @@ import 'package:database_query_builder/query_builder.dart';
 
 class SelectStatements {
   static Future<void> select() async {
-    await DB.table('people').get();
-    await DB.table('people').select([]).get();
-    await DB.table('people').select(['first_name', 'age']).get();
-    await DB.table('people').select(['first_name', 'age']).where('id', 5).get();
+    // Select simple
+    final sql1 = DB.table('people').toSQL();
+    print(sql1);
+
+    // Select simple with not parameters
+    final sql2 = DB.table('people').select([]).toSQL();
+    print(sql2);
+
+    // Select simple with  parameters
+    final sql3 = DB.table('people').select(['first_name', 'age']).toSQL();
+    print(sql3);
+
+    // Select with where
+    final sql4 =
+        DB.table('people').select(['first_name', 'age']).where('id', 5).toSQL();
+    print(sql4);
+
+    // Select nested
+    final sql5 = DB
+        .table('people')
+        .select(['first_name', 'age']).select(['last_name']).toSQL();
+    print(sql5);
+
+    // nested select with repeated parameters
+    final sql6 = DB
+        .table('people')
+        .select(['first_name', 'age']).select(['age']).toSQL();
+    print(sql6);
+
+    final sql6Data = await DB
+        .table('people')
+        .select(['first_name', 'age']).select(['age']).get();
+    print(sql6Data);
   }
 }
