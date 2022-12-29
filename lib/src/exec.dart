97b4811def64389fa7a677a8472db842248df93e
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2022-12-29 03:30:09 -0500

    access DataSQL

--- lib/src/exec.dart
+++ lib/src/exec.dart
@@ -1,6 +1,7 @@
+import 'package:database_query_builder/src/data.dart';
 import 'package:database_query_builder/src/sql_query.dart';
 
-class Execute with Exec {}
+class Execute with Exec, DataSQL {}
 
 class Exec {
   Future<int> save() async {
