1d688b65ee85ead7800fc8f9ef2fccfdedae3344
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2022-12-30 02:58:05 -0500

    add test where

--- test/query_builder_test.dart
+++ test/query_builder_test.dart
@@ -1,1 +1,167 @@
-void main() {}
+import 'package:database_query_builder/query_builder.dart';
+import 'package:database_query_builder/src/select.dart';
+import 'package:database_query_builder/src/where.dart';
+import 'package:test/test.dart';
+
+void main() {
+  DB.conexion(
+    databaseName: 'dartpay',
+    port: 5442,
+  );
+
+  group('SELECT', () {
+    test('Direct', () async {
+      final query = DB.table('people').select(['age']);
+
+      final t1 = await query.get();
+      final t2 = query.toSQL();
+
+      expect(query, isA<Select>());
+      expect(t1, isA<List<Map<String, dynamic>>>());
+      expect(t2, equals('SELECT age FROM people'));
+    });
+
+    test('Where', () async {
+      final query = DB.table('people').select(['age']).where('age', 10);
+
+      final t1 = await query.get();
+      final t2 = query.toSQL();
+
+      expect(query, isA<Where>());
+      expect(t1, isA<List<Map<String, dynamic>>>());
+      expect(
+        t2,
+        equals('SELECT age FROM people WHERE age=10'),
+      );
+    });
+
+    test('Where doble', () async {
+      final query = DB
+          .table('people')
+          .select(['age'])
+          .where('age', WhereType.equal, 10)
+          .whereIN('age', [10, 20])
+          .where('first_name', '=', 'Jean');
+
+      final t1 = await query.get();
+      final t2 = query.toSQL();
+
+      expect(query, isA<Where>());
+      expect(t1, isA<List<Map<String, dynamic>>>());
+      expect(
+        t2,
+        equals(
+          'SELECT age FROM people WHERE age=10 AND age IN (10, 20) AND '
+          "first_name='Jean'",
+        ),
+      );
+    });
+  });
+
+  group('UPDATE', () {
+    test('Direct', () async {
+      final query = DB.table('people').update({'age': 15});
+
+      final t1 = await query.save();
+      final t2 = query.toSQL();
+
+      expect(query, isA<WhereExec>());
+      expect(t1, isA<int>());
+      expect(t2, equals('UPDATE people SET age=15'));
+    });
+
+    test('Where', () async {
+      final query = DB
+          .table('people')
+          .update({'age': 15})
+          .where('age', '10')
+          .where('first_name', '=', 'Jean');
+
+      final t1 = await query.save();
+      final t2 = query.toSQL();
+
+      expect(query, isA<WhereExec>());
+      expect(t1, isA<int>());
+      expect(
+        t2,
+        equals(
+          "UPDATE people SET age=15 WHERE age='10' AND first_name='Jean'",
+        ),
+      );
+    });
+
+    test('Where IN', () async {
+      final query =
+          DB.table('people').update({'age': 15}).whereIN('age', [10, 20]);
+
+      final t1 = await query.save();
+      final t2 = query.toSQL();
+
+      expect(query, isA<WhereExec>());
+      expect(t1, isA<int>());
+      expect(t2, equals('UPDATE people SET age=15 WHERE age IN (10, 20)'));
+    });
+
+    test('Where doble', () async {
+      final query = DB
+          .table('people')
+          .update({'age': 15})
+          .where('age', WhereType.equal, 10)
+          .whereIN('age', [10, 20]);
+
+      final t1 = await query.save();
+      final t2 = query.toSQL();
+
+      expect(query, isA<WhereExec>());
+      expect(t1, isA<int>());
+      expect(
+        t2,
+        equals('UPDATE people SET age=15 WHERE age=10 AND age IN (10, 20)'),
+      );
+    });
+  });
+
+  group('WHERE IN', () {
+    test('Direct', () async {
+      final query = DB.table('people').whereIN('age', [10, 20]);
+
+      final t1 = await query.get();
+      final t2 = query.toSQL();
+
+      expect(query, isA<Where>());
+      expect(t1, isA<List<Map<String, dynamic>>>());
+      expect(t2, equals('SELECT * FROM people WHERE age IN (10, 20)'));
+    });
+
+    test('Select', () async {
+      final query = DB.table('people').select(['age', 'first_name']).whereIN(
+        'age',
+        [10, 20],
+      );
+
+      final t1 = await query.get();
+      final t2 = query.toSQL();
+
+      expect(query, isA<Where>());
+      expect(t1, isA<List<Map<String, dynamic>>>());
+      expect(
+        t2,
+        equals(
+          'SELECT age, first_name FROM people WHERE age IN (10, 20)',
+        ),
+      );
+    });
+
+    test('Update', () async {
+      final query =
+          DB.table('people').update({'age': 15}).whereIN('age', [10, 20]);
+
+      final t1 = await query.save();
+      final t2 = query.toSQL();
+
+      expect(query, isA<WhereExec>());
+      expect(t1, isA<int>());
+      expect(t2, equals('UPDATE people SET age=15 WHERE age IN (10, 20)'));
+    });
+  });
+}
--- test/querys/where_test.dart
+++ test/querys/where_test.dart
@@ -1,12 +1,23 @@
 import 'package:database_query_builder/query_builder.dart';
+import 'package:database_query_builder/src/where.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Table->update empty', () {
+  test('Table->where empty', () {
     try {
       DB.table('people').where();
     } catch (error) {
       expect(error, 'required parameters for WHERE');
     }
+  });
+
+  test('Table->whereIN empty', () {
+    final instance = DB.table('people').whereIN('age', [10, 20, 30]);
+    expect(instance, isA<Where>());
+  });
+
+  test('Table->whereIN empty', () {
+    final instance = DB.table('people').whereIN('age', [10, 20, 30]).toSQL();
+    expect(instance, equals('SELECT * FROM people WHERE age IN (10, 20, 30)'));
   });
 }
